52c46d7ae7d9f54fb0cd9c57a54e460bf92e8a0d
Bill <billy8407@gmail.com>
2020-10-27 23:35:32 +0800

    test

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
 import asyncio
-import time
 
-async def test(delay, reply):
-    await asyncio.sleep(delay)
-    print(reply)
-
+async def nested():
+    return 42
 
 async def main():
-    task1 = asyncio.create_task(test(1, 'hello'))
-    task2 = asyncio.create_task(test(2, "world"))
-    print(f"startd time {time.strftime('%X')}")
-    await task1
-    await task2
-    print(f"end time {time.strftime('%X')}")
-
+    task1 = asyncio.create_task(nested())
+    print(await task1)
 asyncio.run(main())
